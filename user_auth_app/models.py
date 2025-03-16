8a8feb6b157595b29addefe03ee594553dbd7cfa
Christoph <c.wagner.germany@gmail.com>
2025-03-16 16:16:30 +0100

    script

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Erweiterung des Standard-Benutzermodells von Django.
+    """
+    email_confirmed = models.BooleanField(default=False)
+    confirmation_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.email
