581990c4ca6ff48d7b05612647f789f0cef3ad12
Christoph <c.wagner.germany@gmail.com>
2025-03-23 14:36:39 +0100

    sign-up

--- user_auth_app/signals.py
+++ user_auth_app/signals.py
@@ -7,7 +7,7 @@ from django.conf import settings
 @receiver(post_save, sender=CustomUser)
 def send_confirmation_email(sender, instance, created, **kwargs):
     if created:  # Nur ausführen, wenn ein neuer Benutzer erstellt wurde
-        confirmation_link = f"{settings.FRONTEND_URL}/signup/{instance.confirmation_token}"
+        confirmation_link = f"{settings.FRONTEND_URL}/sign-up/{instance.confirmation_token}"
         send_mail(
             'Email confirmation',
             f'Please click on the following link to confirm your e-mail address: {confirmation_link}',
