a79ada7a96e6cf1abd35df5f21dcda80021ad60a
Christoph <c.wagner.germany@gmail.com>
2025-03-28 18:35:51 +0100

    mail

--- user_auth_app/signals.py
+++ user_auth_app/signals.py
@@ -19,17 +19,35 @@ def send_confirmation_email(sender, instance, created, **kwargs):
         )
         html_message = f"""
         <html>
-        <head>
-        <title>Confirm your email</title>
-        </head>
-        <body>
-        <img src="http://videoflix.cw-coding.de/mail_logo/videoflix-logo.png" alt="Videoflix Logo">
-        <p>Click the following link to confirm your email:</p>
-        <a href="{confirmation_link}">Confirm Email</a>
-        <p>Or copy and paste this link into your browser:</p>
-        <p>{confirmation_link}</p>
-        </body>
-        </html>
+  <head>
+    <title>Confirm your email</title>
+  </head>
+  <body style="color: #000;">
+    <header style="display: flex; justify-content: center;">
+      <img
+        src="http://videoflix.cw-coding.de/mail_logo/videoflix-logo.png"
+        alt="Videoflix Logo"
+      />
+    </header>
+    <p>
+      Thank you for registering with
+      <a href="https://videoflix.cw-coding.de" target="_blank">Videoflix</a>. To
+      complete your registration and verify your email address, please click the
+      link below:
+    </p>
+    <button href="{confirmation_link}"
+      style="font-weight: 700; font-size: 18px; letter-spacing: 0.75px; color: white; background-color: #2e3edf; padding: 12px 24px; border: 0; border-radius: 50px; white-space: nowrap; cursor: pointer;"
+    >
+      Activate account
+    </button>
+    <p>
+      If you did not create an account with us, please disregard this email.
+    </p>
+    <p>Best regards,</p>
+    <p>Your Videoflix Team.</p>
+  </body>
+</html>
+
         """
         text_message = f"Please click on the following link to confirm your email address: {confirmation_link}"
         send_mail(
