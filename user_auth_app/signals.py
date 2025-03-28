e6a0e1157d9080cd47a2527b0add0dc12c95b427
Christoph <c.wagner.germany@gmail.com>
2025-03-28 18:16:27 +0100

    logo working

--- user_auth_app/signals.py
+++ user_auth_app/signals.py
@@ -1,4 +1,3 @@
-import base64
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import CustomUser
@@ -18,14 +17,12 @@ def send_confirmation_email(sender, instance, created, **kwargs):
         confirmation_link = (
             f"{settings.FRONTEND_URL}/sign-up/{instance.confirmation_token}"
         )
-        # Bild in Base64 codieren
         html_message = f"""
         <html>
         <head>
-        <title>Email Confirmation</title>
+        <title>Confirm your email</title>
         </head>
         <body>
-        <h1>Please confirm your email address</h1>
         <img src="http://videoflix.cw-coding.de/mail_logo/videoflix-logo.png" alt="Videoflix Logo">
         <p>Click the following link to confirm your email:</p>
         <a href="{confirmation_link}">Confirm Email</a>
