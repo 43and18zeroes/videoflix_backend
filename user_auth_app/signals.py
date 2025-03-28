63444506439fff3b9ff4e9897239a424a0b0d04e
Christoph <c.wagner.germany@gmail.com>
2025-03-28 18:12:52 +0100

    encodedlogo2

--- user_auth_app/signals.py
+++ user_auth_app/signals.py
@@ -11,31 +11,32 @@ from .models import CustomUser
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 @receiver(post_save, sender=CustomUser)
 def send_confirmation_email(sender, instance, created, **kwargs):
     if created:
-        confirmation_link = f"{settings.FRONTEND_URL}/sign-up/{instance.confirmation_token}"
+        confirmation_link = (
+            f"{settings.FRONTEND_URL}/sign-up/{instance.confirmation_token}"
+        )
         # Bild in Base64 codieren
-        with open("media/img/brand/videoflix-logo.jpg", "rb") as image_file:
-            encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
         html_message = f"""
         <html>
         <head>
-          <title>Email Confirmation</title>
+        <title>Email Confirmation</title>
         </head>
         <body>
-          <h1>Please confirm your email address</h1>
-          <img src="data:image/png;base64,{encoded_image}" alt="Beispielbild">
-          <p>Click the following link to confirm your email:</p>
-          <a href="{confirmation_link}">Confirm Email</a>
-          <p>Or copy and paste this link into your browser:</p>
-          <p>{confirmation_link}</p>
+        <h1>Please confirm your email address</h1>
+        <img src="http://videoflix.cw-coding.de/mail_logo/videoflix-logo.png" alt="Videoflix Logo">
+        <p>Click the following link to confirm your email:</p>
+        <a href="{confirmation_link}">Confirm Email</a>
+        <p>Or copy and paste this link into your browser:</p>
+        <p>{confirmation_link}</p>
         </body>
         </html>
         """
         text_message = f"Please click on the following link to confirm your email address: {confirmation_link}"
         send_mail(
-            'Email confirmation',
+            "Email confirmation",
             text_message,
             settings.DEFAULT_FROM_EMAIL,
             [instance.email],
