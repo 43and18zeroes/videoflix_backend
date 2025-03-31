7974f73f19f92bc5f1681b2be5f03700c0240fc0
Christoph <c.wagner.germany@gmail.com>
2025-03-31 16:59:42 +0200

    url

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('set-password/', views.set_password, name='set_password'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', views.LoginView.as_view(), name='login')
 ]
