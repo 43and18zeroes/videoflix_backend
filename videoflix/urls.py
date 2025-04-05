ea27631030dd6707446644c6b5ff3ec2f0c886d8
Christoph <c.wagner.germany@gmail.com>
2025-04-05 10:52:07 +0200

    django.contrib.auth.urls

--- videoflix/urls.py
+++ videoflix/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('user_auth_app.api.urls')),
     path("api/v1/auth/", include("dj_rest_auth.urls")),
-    path('api/v1/auth/', include('django.contrib.auth.urls')),
 ]
