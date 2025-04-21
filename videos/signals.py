d9988d39ae7d9df591bda7ee4d8061e256ce52ed
Christoph <c.wagner.germany@gmail.com>
2025-04-21 15:00:38 +0200

    convert

---
+++ videos/signals.py
@@ -0,0 +1,13 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+from .models import Video
+from .tasks import convert_480p
+
+
+@receiver(post_save, sender=Video)
+def video_post_save(sender, instance, created, **kwargs):
+    print('Video saved')
+    if created:
+        print('New video created')
+        source_path = instance.video_file.path
+        convert_480p(source_path)
---
+++ videos/tasks.py
@@ -0,0 +1,5 @@
+import subprocess
+
+def convert_480p(source):
+    target =  source + '-480p.mp4'
+    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
