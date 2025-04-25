1715c935b085a4ad0bf841d6f97e7caf26d52c47
Christoph <c.wagner.germany@gmail.com>
2025-04-25 18:54:58 +0200

    desc

--- videos/models.py
+++ videos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Video(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField(max_length=255)
+    description = models.TextField(max_length=255, blank=True)
     upload_date = models.DateTimeField(auto_now_add=True)
     video_file = models.FileField(upload_to='videos/')
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
