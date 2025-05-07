87706c711052cb80c8ee3575d1e02c94c5cb5b1d
Christoph <c.wagner.germany@gmail.com>
2025-05-07 16:42:37 +0200

    debug true

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#e0yu)w0o$wsu67!(4@-=708jk$*_fzo@*g077&7#c+w)e^(@@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
