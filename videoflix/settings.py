bffcf342ebfc3bbc907e86109189ebd62124301a
Christoph <c.wagner.germany@gmail.com>
2025-05-09 17:43:25 +0200

    34.89.209.90

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-#e0yu)w0o$wsu67!(4@-=708jk$*_fzo@*g077&7#c+w)e^(@@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.40.18.218', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['34.89.209.90', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -156,8 +156,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
-    "https://34.40.18.218",
-    "http://34.40.18.218",
+    "https://34.89.209.90",
+    "http://34.89.209.90",
 ]
 
 FRONTEND_URL = "http://localhost:4200"
